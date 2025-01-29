e4b72fbe3ac879e6de9f24365f7fac2ee72be851
Despacito64 <meenakshikumari3429@gmail.com>
2025-01-29 11:23:44 +0000

    commit

--- Basics/datatypes.js
+++ Basics/datatypes.js
@@ -2,7 +2,25 @@
 /*
 String, Number, Boolean, null, undefined, Symbol, BigInt
 */
+/*
+JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
 
+Return type of variables in JavaScript
+1) Primitive Datatypes ->> use stack memory
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes ->> use heap memory
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+ */
 /*
 Reference
 Array, Objects, Functions
@@ -13,5 +31,20 @@ let myObj={
 };
 const myFunction=function(){
     console.log("helllooo");
-    
+     
 }
+let subjectOne="maths"
+let subjectTwo=subjectOne
+subjectTwo="english"
+
+/*
+only value of subjectTwo is changed as a copy of subjectOne is passed to it
+*/
+let userOne={
+    userName:"harsh",
+};
+let userTwo=userOne
+userTwo.userName="naman"
+/*
+value in userOne will also be changed as objects are stored in heap memory and it will passed as reference
+*/
--- Basics/string.js
+++ Basics/string.js
@@ -1,2 +1,13 @@
 //string concatenation
-console.log("hey"+50+"@"+"gmail.com")
+const Name="hey"
+const x=50
+console.log(Name+x+"@"+"gmail.com")
+
+//string interpolation
+console.log(`hello my name is ${Name} and my count is ${x}`);
+console.log(Name[0])
+
+//another way to declare strings
+const gameName=new String('harshit')
+//can access various string methods
+ 
